c6b8778819514fbd2feb3f92fb3c0699e5b7ece2
florinhegedus <florinxhgds@gmail.com>
2026-06-02 22:55:03 +0300

    add step to extract_frames.py

--- examples/extract_frames.py
+++ examples/extract_frames.py
@@ -2,14 +2,19 @@ import cv2
 from pathlib import Path
 
 
-vid = cv2.VideoCapture(rf"C:\D_partition\projects\render-lab\scenes\sufragerie-f\GX010313.MP4")
-outdir = Path(rf"C:\D_partition\projects\render-lab\scenes\sufragerie-f\frames")
+if __name__ == "__main__":
+    vid = cv2.VideoCapture(rf"C:\D_partition\projects\render-lab\scenes\sufragerie-f\GX010313.MP4")
+    outdir = Path(rf"C:\D_partition\projects\render-lab\scenes\sufragerie-f\frames-10")
+    outdir.mkdir(exist_ok=True)
 
-count, success = 0, True
-while success:
-    success, image = vid.read() # Read frame
-    if success: 
-        cv2.imwrite(outdir / f"{count:06d}.jpg", image) # Save frame
-        count += 1
+    step = 10
+    count, success = 0, True
+    while success:
+        success, image = vid.read() # Read frame
+        if success: 
+            if count % step == 0:
+                cv2.imwrite(outdir / f"{count:06d}.jpg", image) # Save frame
+                print(f"Saved {count:06d}.jpg")
+            count += 1
 
-vid.release()
+    vid.release()
