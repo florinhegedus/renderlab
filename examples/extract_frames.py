0b419b6c777cc6b3fcbd5a19c44fa57c3f51d934
florinhegedus <florinxhgds@gmail.com>
2026-06-06 14:11:03 +0300

    update extract_frames.py with total number of frames info

--- examples/extract_frames.py
+++ examples/extract_frames.py
@@ -3,12 +3,14 @@ from pathlib import Path
 
 
 if __name__ == "__main__":
-    video_path = Path(rf"C:\D_partition\projects\render-lab\scenes\birou-f-2\GX010322.MP4")
+    video_path = Path(rf"C:\D_partition\projects\render-lab\scenes\living-pinhole\GX010328.MP4")
     vid = cv2.VideoCapture(video_path)
+    length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+    step = 10
+    print(f"{length} frames in total, extracting {length // step}...")
     outdir = video_path.parent / "images"
     outdir.mkdir(exist_ok=True)
 
-    step = 10
     count, success = 0, True
     while success:
         success, image = vid.read() # Read frame
