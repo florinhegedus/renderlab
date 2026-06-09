d838b5aa1f7461a8f806e108d7ecc2c4865eedd5
florinhegedus <florinxhgds@gmail.com>
2026-06-09 23:34:14 +0300

    update install_colmap.sh

--- scripts/install_colmap.sh
+++ scripts/install_colmap.sh
@@ -11,39 +11,60 @@ export CUDACXX=/usr/local/cuda/bin/nvcc
 export CUDA_HOME=/usr/local/cuda
 export CUDA_ROOT=/usr/local/cuda
 
-# get CUDA version
-CUDA_MAJOR=$(nvidia-smi | grep -Po 'CUDA Version: \K[0-9]+')
-CUDA_MINOR=$(nvidia-smi | grep -Po 'CUDA Version: [0-9]+\.\K[0-9]+')
+# conda
+CONDA_PATH="/opt/miniforge3/etc/profile.d/conda.sh"
 
-conda create -n colmap python=3.12
-conda config --add channels conda-forge
-conda config --set channel_priority strict
-conda install \
+if [ -f "$CONDA_PATH" ]; then
+    source "$CONDA_PATH"
+else
+    echo "Error: conda.sh not found at $CONDA_PATH"
+    exit 1
+fi
+
+# install colmap dependencies
+cd /workspace
+sudo apt-get install -y \
+    git \
     cmake \
-    ninja \
-    boost \
-    ccache \
-    eigen \
-    openimageio \
-    curl \
-    metis \
-    glog \
-    gtest \
-    ceres-solver \
-    suitesparse \
-    qt \
-    glew \
-    sqlite \
-    cgal-cpp \
-    mesa-libgl-devel-cos7-x86_64 \
-    cuda-compiler==$CUDA_MAJOR.$CUDA_MINOR.* \
-    cuda-cudart-dev \
-    cuda-nvrtc-dev \
-    libcurand-dev
+    ninja-build \
+    build-essential \
+    libboost-program-options-dev \
+    libboost-graph-dev \
+    libboost-system-dev \
+    libeigen3-dev \
+    libopenimageio-dev \
+    openimageio-tools \
+    libmetis-dev \
+    libglm-dev \
+    libgoogle-glog-dev \
+    libgtest-dev \
+    libgmock-dev \
+    libsqlite3-dev \
+    libglew-dev \
+    qt6-base-dev \
+    libqt6opengl6-dev \
+    libqt6openglwidgets6 \
+    libcgal-dev \
+    libceres-dev \
+    libsuitesparse-dev \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    libmkl-full-dev \
+    imagemagick \
+    nvidia-cuda-toolkit \
+    nvidia-cuda-toolkit-gcc
+sudo mkdir -p /usr/include/opencv4
 
+# install colmap
 git clone https://github.com/colmap/colmap.git
 cd colmap
 mkdir build
 cd build
-cmake .. -GNinja
-ninja
+cmake .. -GNinja \
+    -DCUDA_ENABLED=ON \
+    -DGUI_ENABLED=OFF \
+    -DOPENGL_ENABLED=OFF \
+    -DCOLMAP_FIND_QUIETLY=ON \
+    -DCMAKE_CUDA_ARCHITECTURES=native
+ninja -j$(nproc)
+sudo ninja install
